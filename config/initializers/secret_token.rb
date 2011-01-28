f7a701ca41e0c480c74f3fbc93215f3175cfbe9f
Paul Heinemann <junkemailaddress3@gmail.com>
2011-01-28 12:13:51 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Venuable::Application.config.secret_token = '58cb24bb8966dacf8dcf8b15d88c83cd47aed5e2e1ec28e8d7e661409c29ab9caac577bcf522fd6775090384245fadf931f7463cfc2e0d9a7ad66a4766ad0244'
